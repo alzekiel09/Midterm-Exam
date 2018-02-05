a2db949844f26c881f9627f73107b16f97105764
alzekiel09 <aixonairashii@gmail.com>
2018-02-05 22:48:40 +0800

    linked in firestore api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: 'APIKEY',
-        authDomain: 'DEV-APP.firebaseapp.com',
-        databaseURL: 'https://DEV-APP.firebaseio.com',
-        projectId: 'DEV-APP',
-        storageBucket: 'DEV-APP.appspot.com',
-        messagingSenderId: '123456789'
+        apiKey: "AIzaSyB_UuB32iILcQDofMYTg3jQCb7n0tnMjnk",
+		authDomain: "firestarter-f3263.firebaseapp.com",
+		databaseURL: "https://firestarter-f3263.firebaseio.com",
+		projectId: "firestarter-f3263",
+		storageBucket: "firestarter-f3263.appspot.com",
+		messagingSenderId: "551493428002"
     }
 };
